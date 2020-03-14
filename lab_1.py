14df27f745a5f0c261d1688ccfff26296ef841a1
Arteom27 <61755111+Arteom27@users.noreply.github.com>
2020-03-14 10:16:26 +0200

    Add files via upload

---
+++ lab_1.py
@@ -0,0 +1,26 @@
+#ЗАДАНИЕ 1
+a='Hello,World'
+print(a)
+a='Маша+Петя=Любовь'
+print(a)
+x='3+4'
+print(x)
+print(3+4)
+#ЗАДАНИЕ 2
+print(type('Hello,World'))
+print(type(3+4))
+print(type(3/4))
+print(type([1,2,5,10,100]))
+#ЗАДАНИЕ 3 
+print(3%2)
+print(31%12)
+print(3**3,4)
+#ЗАДАНИЕ 4
+a=[1,5,'Good','Bad']
+b=[9,'Blue','Red',11]
+print(5+11)
+print('Good'+'Red')
+print(1*9)
+print(5**11)
+c=a+b
+print(c)
